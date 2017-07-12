44c413e9bdb696d8081b5879affc26abcbba69cf
李明 <liming3@100tal.com>
2017-07-12 17:30:22 +0800

    add

---
+++ angular/moocAngular/js/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '<selector>',
+  templateUrl: '<template.html>',
+})
+export class <Name>Component implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+}
