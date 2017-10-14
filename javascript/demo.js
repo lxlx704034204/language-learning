c53163e650e2238f975de261b8b5b694edbe6dff
LM <LM@xiaomingdeiMac.local>
2017-10-14 22:54:13 +0800

    home

---
+++ javascript/demo.js
@@ -0,0 +1,168 @@
+function add() {
+  console.log('123');
+}
+
+mukewang
+
+aiqiyyij
+
+datianyang
+
+wangzherngyao
+
+wangzherongyao
+
+fengtian
+han lan da
+
+wozhidao ya
+
+henhaoyongde  xiaojianpan
+
+
+
+       add();
+
+require('''"""""++++====')/////?????.....>>>>><><><>MNBVV ADF ADFJKLADJFKLLKJFASKLJKL
+fuklalkjdfklj lkajdfklj add good():
+
+switch(add){
+  case 1:
+  return a;
+  break
+
+
+
+
+
+liudeehuahuijiadelu
+
+huijialde lu function geiroilkjdskl
+
+haode meiyou wenti
+
+dajiahao caishi zhendehao
+
+kaishi
+
+
+git add --ADFJKLADJFKLLKJFASKLJKL
+git commit -m 'ad'
+git push origin master
+git pull-right
+
+
+
+bi
+biyibanjianpan hui haoyonghenduo
+
+zhge shi qing
+haode
+meiyou wenti
+zhgeishi
+wo lai gaoding
+haode meiyou wenti
+
+
+@akjaklj
+kaishi zouqilai
+kaishi zouqilai
+zhgeshiqing wozhidao le
+
+
+!§§§§§§11111!!@@@@@
+
+wozhidao zhge shiqingheni meihyou guanli
+zhegeshi while
+do
+zhendehaoxiangni zhege fanzhegeshiqing heni guanxi buda
+wozhidao  zhgeyangzi de shiqing meiyou shenmekeyizhuzhi  youxijianpan
+
+zhge juedui de meiuyou wenti de wanshang jiukeyi qu xifangzi shuijiao le
+zhge wo zhenxinde butai zaihu
+
+duiyu shiwude kaishi  wo juede zhge meiyou shebnme keyi zudang
+meiyou shenme keyi zudang  nidui  ziyou de xiangwang tianmaxingkongdeshengya
+nide xin  liaowuqiangua
+
+
+function add(){
+  alert('add');
+  console.log(bug)
+}
+
+中华民族
+
+民族复兴
+
+白粉 吸毒
+
+啊啊啊啊啊啊啊啊啊啊啊啊啊的深刻理解  啊的深刻了房间啊的深刻理解么 啊圣诞快乐姐妹
+圣诞节
+快乐
+明天快乐
+
+阴天快快
+好样的
+啥也别说了
+我胖
+哪怕摩纳哥
+
+摩的哥
+
+水生
+
+funation 啊的法律框架
+啊但是离开起飞啦快速的减肥科技来克己复礼卡家的法律框架
+阿斯顿风口浪尖啦kkjsflkjads【2偶哦UI啊但是烦恼
+啊的水分即可
+啊的深刻了房间离开家asdfk 阿斯顿看风景了；k
+ 阿斯顿看了放弃
+
+
+ 啊上岛咖啡就离开
+
+ 就啊的深刻理解离开家就看了看理解a【大煞风景离开了
+
+  啊但是克己复礼；看
+  啊的深刻了放弃离开家
+   阿斯顿离开家lk
+
+
+   啊的方式风口浪尖lkkjkladsi俄4南大舒服了；自己看啦圣诞快乐姐妹klj
+3.js
+
+
+ 啊的深刻理解
+
+
+ 阿斯顿看风景
+ jk
+ js
+
+ 。js
+
+
+3.js
+
+function.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+hai 就啊美女烂打汉兰达
+
+仿佛那次哦
