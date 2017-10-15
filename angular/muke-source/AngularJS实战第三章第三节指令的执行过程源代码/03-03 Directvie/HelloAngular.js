46d00cbf5f52852df79868c6c89e7cc502025d08
lxlx704034204 <704034204@qq.com>
2017-10-15 22:36:45 +0800

    <div>Hi everyone!</div>

--- angular/muke-source/AngularJS实战第三章第三节指令的执行过程源代码/03-03 Directvie/HelloAngular.js
+++ angular/muke-source/AngularJS实战第三章第三节指令的执行过程源代码/03-03 Directvie/HelloAngular.js
@@ -1,8 +1,8 @@
-var myModule = angular.module("MyModule", []);
-myModule.directive("hello", function() {
+var myModule = angular.module("MyModule", []);//一切从module开始 包括directive
+myModule.directive("hello"  , function() {   //利用指令  自定义html中的<hello></hello>标签
     return {
         restrict: 'E',
-        template: '<div>Hi everyone!</div>', 
+        template: '<div>Hi everyone!</div>', // "hello" 最终被 这里替换显示在page
         replace: true
     }
 });
